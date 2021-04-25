5fa1a2fdfffc100bd6a7417522554157cd17bd52
Miri <79663276+mrsperez20@users.noreply.github.com>
2021-04-25 09:59:04 -0500

    Add files via upload

---
+++ updates.swift
@@ -0,0 +1,21 @@
+//
+//  updates.swift
+//  VACC1
+//
+//  Created by administrator on 4/21/21.
+//
+
+import SwiftUI
+
+struct updates: View {
+    var body: some View {
+        Text("Vaccine Updates")
+
+    }
+}
+
+struct updates_Previews: PreviewProvider {
+    static var previews: some View {
+        updates()
+    }
+}
