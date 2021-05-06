c55ab66faae14e0f9c65b3f9b2c7e3e11a831fb4
Miri <79663276+mrsperez20@users.noreply.github.com>
2021-05-06 17:38:11 -0500

    Add files via upload

---
+++ VACC1App.swift
@@ -0,0 +1,17 @@
+//
+//  VACC1App.swift
+//  VACC1
+//
+//  Created by administrator on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct VACC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
