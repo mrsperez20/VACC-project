e9230657730c8baa0600501b54a1149a2a2517e4
Miri <79663276+mrsperez20@users.noreply.github.com>
2021-05-13 05:57:50 -0500

    Add files via upload

---
+++ LocationINFOApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationINFOApp.swift
+//  LocationINFO
+//
+//  Created by M Perez on 4/29/21.
+//
+
+import SwiftUI
+
+@main 
+struct LocationINFOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
